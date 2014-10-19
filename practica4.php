2ea6f6252b32a56d0fc59555596445c30b235ba2
bettylu <btty.deluna@gmail.com>
2014-10-19 17:24:24 -0500

    practica 4

---
+++ practica4.php
@@ -0,0 +1,142 @@
+<html>
+<head>
+<title>Practica4</title>
+</head>
+//practica
+   <body>
+     <form>
+	<table>
+     <tr>
+<?php
+
+$datos=array(
+	array(
+	id1=>'1';
+	'producto1'=>'pan';
+	'precio1'=>10;
+	);
+<?
+	<td>
+	<?php echo $datos ['id1']; <?
+	 </td>
+	<td>
+	<?php echo $datos ['producto1']; <? 
+	</td>
+	<td>
+	<?php echo $datos ['precio1'];<? 
+	</td>
+	<td> 
+	<a href="Editar.html">Editar</a>    
+	<a href="Eliminar.html">Eliminar</a>
+	</td> 
+	<td>                                  
+	</td>
+    </tr>
+
+    <tr>
+<?php
+	array(
+	id2=>'2';
+	'producto2'=>'refresco';
+	'precio2'=>10;
+	);
+<?
+	<td>
+	<?php echo $datos ['id2']; <? 
+	</td>
+	<td>
+	<?php echo $datos ['producto2']; <? 
+	</td>
+	<td>
+	<?php echo $datos ['precio2'];<? 
+	</td>
+	<td> 
+	<a href="Editar.html">Editar</a>    
+	<a href="Eliminar.html">Eliminar</a>
+	</td> 
+	<td>                                  
+	</td>
+    </tr>
+	
+    <tr>
+<?php
+	array(
+	id3=>'3';
+	'producto3'=>'Verduras';
+	'precio3'=>2;
+	);
+<?
+	<td>
+	<?php echo $datos ['id3']; <? 
+	</td>
+	<td>
+	<?php echo $datos ['producto3']; <? 
+	</td>
+	<td>
+	<?php echo $datos ['precio3'];<? 
+	</td>
+	<td> 
+	<a href="Editar.html">Editar</a>    
+	<a href="Eliminar.html">Eliminar</a>
+	</td> 
+	<td>                                  
+	</td>
+    </tr>
+
+    <tr>
+<?php
+	array(
+	id4=>'4';
+	'producto4'=>'frutas';
+	'precio4'=>20;
+	);
+<?
+	<td>
+	<?php echo $datos ['id4']; <? 
+	</td>
+	<td>
+	<?php echo $datos ['producto4']; <? 
+	</td>
+	<td>
+	<?php echo $datos ['precio4'];<? 
+	</td>
+	<td> 
+	<a href="Editar.html">Editar</a>    
+	<a href="Eliminar.html">Eliminar</a>
+	</td> 
+	<td>                                  
+	</td>
+    </tr>
+
+    <tr>
+<?php
+	array(
+	id5=>'5';
+	'producto5'=>'agua';
+	'precio5'=>12;
+	);
+<?
+	<td>
+	<?php echo $datos ['id5']; <? 
+	</td>
+	<td>
+	<?php echo $datos ['producto5']; <? 
+	</td>
+	<td>
+	<?php echo $datos ['precio5'];<? 
+	</td>
+	<td> 
+	<a href="Editar.html">Editar</a>    
+	<a href="Eliminar.html">Eliminar</a>
+	</td> 
+	<td>                                  
+	</td>
+    </tr>
+    </tr>										
+    </tr>				
+	</table>
+      </form>
+    </body>
+</html>
+
+
